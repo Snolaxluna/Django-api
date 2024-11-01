ed66bdd47a22b6f2b118e490486c5c0c4ae3aac3
Snolaxluna <lucassolanich1987@gmail.com>
2024-10-31 21:11:14 -0300

    Add permissions

--- escola/views.py
+++ escola/views.py
@@ -2,17 +2,21 @@ from escola.models import Estudante, Curso, Matricula
 from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
 from rest_framework import viewsets, generics
 
+
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
+
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
+
 class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
 
 class ListaMatriculaEstudante(generics.ListAPIView):
     def get_queryset(self):
@@ -20,9 +24,9 @@ class ListaMatriculaEstudante(generics.ListAPIView):
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
+
 class ListaMatriculasCurso(generics.ListAPIView):
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasCursoSerializer
-    
